83e438cab19851ee84e9b3d12226a41f61b81b7b
ardeshir <ardeshir.org@gmailc.com>
2025-10-25 21:13:54 -0500

    clean up

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    println!("Welcome to the Universe, Anoush's World of Hacks!");
+    println!("\n\n Welcome to the Universe, Anoush's World of Hacks!\n ");
     println!("What did you want me to help you calculate?");
     // Add numbers for x and y 
 
@@ -7,12 +7,12 @@ fn main() {
     let  y: f64 = 260.0;
 
     // Addingtion 
-    println!("Add: \nThe answer is: {}\n", x + y );
+    println!("\n X is {} and Y is {} \n", x , y );
+    println!("Add x + y the answer is: {}\n", x + y );
 
-    println!("Substract: \nThe answer is: {}\n", x - y );
+    println!("Substract x - y answer is: {}\n", x - y );
 
-    println!("Multiply: \nThe answer is: {}\n", x * y );
+    println!("Multiply x * y answer is: {}\n", x * y );
     
-    println!("Divide: \nThe answer is: {}\n", x/y );
-
+    println!("Divide x / y answer is: {}\n", x/y );
 }
