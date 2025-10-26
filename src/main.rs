533d13da06d94b1127d0cb2b1a48006feae5d08c
ardeshir <ardeshir.org@gmailc.com>
2025-10-25 21:04:54 -0500

    clean up

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,18 @@
 fn main() {
     println!("Welcome to the Universe, Anoush's World of Hacks!");
-    println!("What did you want to do next?")
+    println!("What did you want me to help you calculate?");
+    // Add numbers for x and y 
+
+    let  x: f64 = 850.0;
+    let  y: f64 = 260.0;
+
+    // Addingtion 
+    println!("Add: \nThe answer is: {}\n", x + y );
+
+    println!("Substract: \nThe answer is: {}\n", x - y );
+
+    println!("Multiply: \nThe answer is: {}\n", x * y );
+    
+    println!("Divide: \nThe answer is: {}\n", x/y );
+
 }
